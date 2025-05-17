ab9edc87aaab084ef9c8c66598d244b1c2dae877
Cyber0Specter <cyber0specter@gmail.com>
2025-05-17 21:04:03 +0600

    bye

---
+++ datatypes.py
@@ -0,0 +1,36 @@
+a=5
+print("type of a:", type(a))
+b=2.5
+print("type of b:", type(b))
+c="coding"
+print("type of c:", type(c))
+d=True
+print("type of d:", type(d))
+name="HUH"
+age=15
+is_student=True
+weight=38.5
+'Type'
+print("Name :",name)
+print("Data Type of Name is", type(name))
+
+print("Age :",age)
+print("Data Type of Age is", type(age))
+
+print("is_student :",is_student)
+print("Data Type of is_student is", type(is_student))
+
+print("Weight :",weight)
+print("Data Type of Weight is", type(weight))
+
+print("\n After Type Casting ....")
+age=str(age)
+print("Data Type age is", type(age))
+weight= int(weight)
+print(weight)
+print("Data Type Of Weight is", type(weight))
+
+Str ="string"
+print(Str[::3])
+print(Str[0::1])
+print(Str[::-1])
