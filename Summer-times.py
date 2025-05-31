0ff1120c887535da128e0728900cb0bc3f6ae546
Cyber0Specter <Cyber0Specter@gmail.com>
2025-05-31 20:09:51 +0600

    bye

---
+++ Summer-times.py
@@ -0,0 +1,7 @@
+Temperature = int(input("Enter the temperature :-"))
+if Temperature>25:
+    print("Its Summer , So you stay hydrated 🥵")
+else:
+    print("Its Winter , So Its a nice chill weather 😎")
+
+#dont worry miss ill upgrade this
